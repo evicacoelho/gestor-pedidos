cbde3fa04e5e93d2047534c3ef41664238f86843
Emanuelle Coelho <evicacoelho@gmail.com>
2026-08-05 19:17:41 -0300

    first commit: starting fresh

---
+++ backend/src/main/java/br/com/raizesdonordeste/gestor/GestorPedidosApplication.java
@@ -0,0 +1,12 @@
+package br.com.raizesdonordeste.gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorPedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorPedidosApplication.class, args);
+    }
+}
